2efc0cb6f45ac1292e9350f4b62d40fcb9e70acc
miharc <ma429chglp>
2021-03-13 17:37:40 +0000

    upload config

--- config/config.js
+++ config/config.js
@@ -85,7 +85,7 @@ var config = {
 			config: {
 				location: "Galway",
 				locationID: "2964180", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
-				showRainAmount: true,
+				showRainAmount: true, // let see if this works
 				maxNumberOfDays: 6,
 				appid: "0ba99ed2b248d02717d54207c248d03e"
 			}
